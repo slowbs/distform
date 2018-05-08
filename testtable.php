40daee8eb3643532d49e104eb1ae78f014470ea0
slowbs <poison_word@hotmail.com>
2018-05-08 15:57:51 +0700

    แก้ font size

--- testtable.php
+++ testtable.php
@@ -4,6 +4,11 @@
   <title>Bootstrap 4 Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script>
+  .table-condensed{
+  font-size: 10px;
+}
+</script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
@@ -15,40 +20,41 @@
   </head>
   <body>
       <div class="container-fluid">
-  <table class="table">
+  <table class="table table-condensed">
   <thead class="thead-dark">
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
+      <th scope="col">PA/สตป.</th>
+      <th scope="col">ลำดับ</th>
+      <th scope="col" style="font-size:12px">ตัวชี้วัดประเมินผล</th>
+      <th scope="col" style="font-size:12px">เกณฑ์ ปี 2561</th>
+      <th scope="col" style="font-size:12px">แหล่งข้อมูล</th>
+      <th scope="col">สสอ</th>
+      <th scope="col">ระดับ1</th>
+      <th scope="col">ระดับ2</th>
+      <th scope="col">ระดับ3</th>
+      <th scope="col">ระดับ4</th>
+      <th scope="col">ระดับ5</th>
+      <th scope="col" style="font-size:12px">ผลการดำเนินงาน</th>
+      <th scope="col" style="font-size:12px">ค่าคะแนนที่ได้</th>
+      <th scope="col" style="font-size:12px">คะแนนถ่วงน้ำหนัก</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td><input type="text" class="form-control"></td>
-      <td style="width:800px"><input type="text" class="form-control"></td>
-      <td><input type="text" class="form-control"></td>
-      <td><input type="text" class="form-control"></td>
-      <td><input type="text" class="form-control"></td>
-      <td><input type="text" class="form-control"></td>
-      <td><input type="text" class="form-control"></td>
-      <td><input type="text" class="form-control"></td>
-      <td><input type="text" class="form-control"></td>
-      <td><input type="text" class="form-control"></td>
-      <td><input type="text" class="form-control"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td style="width:700px"><input type="text" class="form-control"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
+      <td><input type="text" class="form-control" style="text-align:center"></td>
     </tr>
 
     <tr>
