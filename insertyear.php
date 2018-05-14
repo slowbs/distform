82e6dfe0764b3d36e5f12d1f1190dc75a3701192
slowbs <poison_word@hotmail.com>
2018-05-14 15:55:46 +0700

    ปุ่ม insert add column work

--- insertyear.php
+++ insertyear.php
@@ -10,7 +10,10 @@ include 'db.php';
         foreach($result as $row){
             $newyear = $row['Max(year)']+1;
             //echo $newyear;
-            $sql = "INSERT INTO year (year) VALUES ($newyear)";
+            $sql = "INSERT INTO year (year) VALUES ($newyear); 
+            alter table ap1 add column value_$newyear varchar (10) not null, add column valuekoon_$newyear varchar (10) not null, add column valuegane_$newyear varchar (10) not null;
+            alter table ap2 add column value_$newyear varchar (10) not null, add column valuekoon_$newyear varchar (10) not null, add column valuegane_$newyear varchar (10) not null;
+            alter table ap3 add column value_$newyear varchar (10) not null, add column valuekoon_$newyear varchar (10) not null, add column valuegane_$newyear varchar (10) not null;";
             $stmt = $conn->prepare($sql);
             $stmt->execute();
             header("Location:index.php");
