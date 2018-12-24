462d3ca716871b95ab597a14ea45b7c01301c0da
slowbs <poison_word@hotmail.com>
2018-12-24 08:57:40 +0700

    fin before add viewform
    push for backup before add view form

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 				</h3>
 			</div>
 		<?php endif ?>
-		<!-- logged in user information -->
+		<!-- logged in user information before add viewform-->
 		<div class="profile_info">
 			<img src="images/user_profile.png"  >
 
